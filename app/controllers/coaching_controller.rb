4833329aacd57fe950ddd1a647b2961d14e50e95
Matthieu Martinot <leproudeur@yahoo.fr>
2014-11-24 14:16:57 +0100

    exo 1

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,16 @@
+class CoachingController < ApplicationController
+  def answer
+    @query = params[:query]
+
+    if @query.include?("?") == true
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer ="I don't care, get dressed and go to work"
+    end
+  end
+
+  def ask
+
+  end
+
+end
